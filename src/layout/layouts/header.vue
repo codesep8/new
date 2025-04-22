d6f9b44242d3e5cadaf896fd6201f197c41d286c
Junyoung <codesep8@gmail.com>
2025-04-22 15:23:18 +0000

    d

--- src/layout/layouts/header.vue
+++ src/layout/layouts/header.vue
@@ -18,11 +18,11 @@
             </div>
             <div class="flex gap-4 items-center">
                 <div class="nav-links text-black">
-                    <input class="rounded-l-lg bg-neutral-100 h-10 p-2" placeholder="여기에 검색...">
-                    <button class="h-10 px-1 bg-neutral-100"><i class="fa-solid fa-magnifying-glass"></i></button>
-                    <button class="h-10 px-2 rounded-r-lg bg-neutral-100"><i class="fa-solid fa-chevron-right"></i></button>
+                    <input class="rounded-l-lg bg-neutral-100 h-9 p-2" placeholder="여기에 검색...">
+                    <button class="h-9 px-1 bg-neutral-100"><i class="fa-solid fa-magnifying-glass"></i></button>
+                    <button class="h-9 px-2 rounded-r-lg bg-neutral-100"><i class="fa-solid fa-chevron-right"></i></button>
                 </div>
-                <img class="md:mr-6 inline-block size-10 rounded-full" src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80"/>
+                <img class="md:mr-6 inline-block size-9 rounded-full" src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80"/>
                 <button class="mr-4 md:hidden rounded text-2xl hover:bg-secondary dark:hover:bg-secondary-dark">
                     <i class="fa-solid fa-bars"></i>
                 </button>
