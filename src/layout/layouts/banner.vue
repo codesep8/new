26999c49a77338d6cff04724d7643da685edd7aa
Junyoung <codesep8@gmail.com>
2025-04-22 15:16:52 +0000

    d

--- src/layout/layouts/banner.vue
+++ src/layout/layouts/banner.vue
@@ -1,17 +1,17 @@
 <template>
-    <div v-if="a" class="h-10 bg-yellow-400 dark:bg-yellow-600 text-white flex items-center gap-3">
+    <div v-if="show" class="h-10 bg-yellow-400 dark:bg-yellow-600 text-white flex items-center gap-3">
         <i class="fa-solid fa-triangle-exclamation ml-6"></i>
         <p>notice</p>
     </div>
-    <button @click="setA()">a</button>
+    <button @click="setShow()">toggle</button>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue"
 
-var a = ref(false)
+var show = ref(false)
 
-function setA() {
-    a != a
+function setShow() {
+    show != show
 }
 </script>
