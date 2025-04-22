aa5402354fc6f12dc29b9728d11a1623c0c73098
Junyoung <codesep8@gmail.com>
2025-04-23 00:18:59 +0900

    Update banner.vue

--- src/layout/layouts/banner.vue
+++ src/layout/layouts/banner.vue
@@ -1,17 +1,17 @@
 <template>
-    <div v-if="show" class="h-10 bg-yellow-400 dark:bg-yellow-600 text-white flex items-center gap-3">
-        <i class="fa-solid fa-triangle-exclamation ml-6"></i>
-        <p>notice</p>
-    </div>
-    <button @click="setShow()">toggle</button>
+  <div v-if="show" class="h-10 bg-yellow-400 dark:bg-yellow-600 text-white flex items-center gap-3">
+    <i class="fa-solid fa-triangle-exclamation ml-6"></i>
+    <p>notice</p>
+  </div>
+  <button @click="setShow">toggle</button>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue"
+import { ref } from 'vue'
 
-var show = ref(false)
+const show = ref(false)
 
 function setShow() {
-    show != show
+  show.value = !show.value
 }
 </script>
